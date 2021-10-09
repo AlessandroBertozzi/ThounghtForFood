2fe30ffe65e7ac87a65aeb86b5890f0f62b545a4
Alessandro Bertozzi <nive54@gmail.comnive54@gmail.com>
2021-10-09 19:31:47 +0200

    home

--- scripts/home_functions.js
+++ scripts/home_functions.js
@@ -44,7 +44,7 @@ function open_menu() {
   
 /*---------------1915-----------------*/
     if (x == "anno-1915"){
-      $.getJSON( '.\\Json\\css.json', {
+      $.getJSON( '..\\Json\\css.json', {
         format: "json"
       }).done(function( data ) {
         let y = data["anno-1915"];
@@ -56,7 +56,7 @@ function open_menu() {
 /*---------------2030-----------------*/
 
       } else if (x == "anno-2030") {
-        $.getJSON( '.\\Json\\2030.json', {
+        $.getJSON( '..\\Json\\2030.json', {
           format: "json"
         }).done(function( data ) {
           $("link[about^='change']").remove();
@@ -67,7 +67,7 @@ function open_menu() {
 
       } else if(x == "anno-2021") {
 
-        $.getJSON( '.\\Json\\css.json', {
+        $.getJSON( '..\\Json\\css.json', {
           format: "json"
           }).done(function(data) {
           $("h1[about^='cancel']").remove();
@@ -76,7 +76,7 @@ function open_menu() {
 
       } else {
         
-        $.getJSON( '.\\Json\\css.json', {
+        $.getJSON( '..\\Json\\css.json', {
         format: "json"
         }).done(function( data ) {
         $("h1[about^='cancel']").remove();
