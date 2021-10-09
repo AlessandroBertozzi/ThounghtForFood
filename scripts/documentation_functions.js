2490872076853bf484d3ae252f53d52b5ef021fa
Alessandro Bertozzi <nive54@gmail.comnive54@gmail.com>
2021-10-09 19:23:35 +0200

    documentation

--- scripts/documentation_functions.js
+++ scripts/documentation_functions.js
@@ -27,66 +27,6 @@ function open_menu() {
     })
   
   
-/*----------------CHANGE_YEAR------------------*/
-
-/*---------------2030 SPECIAL BUTTONS -----------------*/
-$(document).on ("change", ".button_2030", function () {
-  var x = this.value;
-  var n = this.name;
-
-    $.getJSON( '\\Json\\css_2030.json', {
-      format: "json"
-    }).done(function( data ) {
-
-      if (x.startsWith("Dyslexia")) {
-        if (x == "Dyslexia_1") {
-          $("#Fast_readings_1").remove();
-          $("head").append("<link rel='stylesheet' class='Dyslexia' id='" + x + "' about='change' type='text/css' href='" + 
-          data[n]["dislessia"] + "' >");
-        } else if (x == "Dyslexia_2") {
-          $("#Fast_readings_2").remove();
-          $("head").append("<link rel='stylesheet' class='Dyslexia' id='" + x + "' about='change' type='text/css' href='" + 
-          data[n]["dislessia"] + "' >");
-        } else {
-          $("#Fast_readings_3").remove();
-          $("head").append("<link rel='stylesheet' class='Dyslexia' id='" + x + "' about='change' type='text/css' href='" + 
-          data[n]["dislessia"] + "' >");
-        };
-        
-
-
-      } else if (x.startsWith("Fast_readings")) {
-        if (x == "Fast_readings_1") {
-          $("#Dyslexia_1").remove();
-          $("head").append("<link rel='stylesheet' class='Fast_readings' id='" + x + "' about='change' type='text/css' href='" + 
-          data[n]["speed_readings"] + "' >");
-        } else if (x == "Fast_readings_2") {
-          $("#Dyslexia_2").remove();
-          $("head").append("<link rel='stylesheet' class='Fast_readings' id='" + x + "' about='change' type='text/css' href='" + 
-          data[n]["speed_readings"] + "' >");
-        } else {
-          $("#Dyslexia_3").remove();
-          $("head").append("<link rel='stylesheet' class='Fast_readings' id='" + x + "' about='change' type='text/css' href='" + 
-          data[n]["speed_readings"] + "' >");
-        }
-
-      } else {
-        if (x == "Normal_1") {
-          $("#Dyslexia_1").remove();
-          $("#Fast_readings_1").remove();
-        } else if (x == "Normal_2") {
-          $("#Dyslexia_2").remove();
-          $("#Fast_readings_2").remove();
-        } else {
-          $("#Dyslexia_3").remove();
-          $("#Fast_readings_3").remove();
-        }
-        
-
-
-      }});
-
-})
 
 
 
@@ -101,7 +41,7 @@ var x = this.getAttribute("value");
 
 /*---------------1915-----------------*/
 if (x == "anno-1915"){
-  $.getJSON( '\\Json\\css.json', {
+  $.getJSON( '..\\Json\\css.json', {
     format: "json"
   }).done(function( data ) {
     let y = data["anno-1915"];
@@ -113,7 +53,7 @@ if (x == "anno-1915"){
 /*---------------2030-----------------*/
 
   } else if (x == "anno-2030") {
-    $.getJSON( '\\Json\\2030.json', {
+    $.getJSON( '..\\Json\\2030.json', {
       format: "json"
     }).done(function( data ) {
       $("link[about^='change']").remove();
@@ -123,17 +63,13 @@ if (x == "anno-1915"){
     })
 
   } else if(x == "anno-2021") {
-
-    $.getJSON( '\\Json\\css.json', {
-      format: "json"
-      }).done(function(data) {
       $("h1[about^='cancel']").remove();
       $("link[about^='change']").remove();
-    })
+    
 
   } else {
     
-    $.getJSON( '\\Json\\css.json', {
+    $.getJSON( '..\\Json\\css.json', {
     format: "json"
     }).done(function( data ) {
     $("h1[about^='cancel']").remove();
