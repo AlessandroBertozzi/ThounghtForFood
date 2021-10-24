89309ee319642fe1d551a6c171f1bf24b1534930
Alessandro Bertozzi <nive54@gmail.comnive54@gmail.com>
2021-10-24 20:13:56 +0200

    changes

--- scripts/documentation_functions.js
+++ scripts/documentation_functions.js
@@ -39,40 +39,36 @@ var x = this.getAttribute("value");
 
 
 
-/*---------------1915-----------------*/
 if (x == "anno-1915"){
-  $.getJSON( '..\\Json\\css.json', {
+  $.getJSON( '..\\Json\\cssDocumentation.json', {
     format: "json"
   }).done(function( data ) {
     let y = data["anno-1915"];
+    $("h1[about^='cancel']").remove();
+    $("#subsection1").prepend(y["title_d"]);
     $("link[about^='change']").remove();
     $('head').append("<link rel='stylesheet' about='change' type='text/css' href='"
       + y.file + "' >");
   })
 
-/*---------------2030-----------------*/
-
-  } else if (x == "anno-2030") {
-    $.getJSON( '..\\Json\\2030.json', {
-      format: "json"
-    }).done(function( data ) {
-      $("link[about^='change']").remove();
-      $('head').append("<link rel='stylesheet' id='Normal' about='change' type='text/css' href='"
-      + data.file + "' >");
-
-    })
 
   } else if(x == "anno-2021") {
+    $.getJSON( '..\\Json\\cssDocumentation.json', {
+      format: "json"
+      }).done(function( data ) {
       $("h1[about^='cancel']").remove();
       $("link[about^='change']").remove();
+      $("#subsection1").prepend(data["title_normal"]);
+      })
     
 
   } else {
     
-    $.getJSON( '..\\Json\\css.json', {
+    $.getJSON( '..\\Json\\cssDocumentation.json', {
     format: "json"
     }).done(function( data ) {
     $("h1[about^='cancel']").remove();
+    $("#subsection1").prepend(data["title_normal"]);
     let y = data[x];
     $("link[about^='change']").remove();
     $('head').append("<link rel='stylesheet' about='change' type='text/css' href='"
@@ -81,6 +77,7 @@ if (x == "anno-1915"){
 };
 open_menu();
 })
+
   
       $(document).on ("click", ".drop-nav", function(e) {
         e.preventDefault()
